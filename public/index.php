7e47dddcdd95760a70e85a06af356c9dfb959a57
Steven200405 <huangsteven2004@gmail.com>
2024-11-13 15:25:14 +0000

    Update

--- public/index.php
+++ public/index.php
@@ -2,10 +2,8 @@
 require("./../Router.php");
 //$request = $_SERVER['REQUEST_URI'];
 $request = new Router();
-echo $request->route();
-
+$request = $request->route();
 if(isset($request)){
-    echo "test";
     switch($request){
         case "Projet1":
             $location = "./../Projet1.php";
---
+++ src/Controller/ContactController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+class ContactController
+{
+    public function create()
+    {
+    }
+
+    public function read()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function delete()
+    {
+    }
+}
