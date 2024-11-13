383477c416092c7810006a51da193093539868b6
Steven200405 <huangsteven2004@gmail.com>
2024-11-13 10:52:32 +0000

    Ajout routeur index.php + .htaccess

--- public/index.php
+++ public/index.php
@@ -1,20 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Filmoteca</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <header>
-        <h1>Welcome to Filmoteca</h1>
-    </header>
-    <main>
-        <p>Your content goes here.</p>
-    </main>
-    <footer>
-        <p>&copy; 2023 Filmoteca. All rights reserved.</p>
-    </footer>
-</body>
-</html>
+<?php
+$request = $_SERVER['REQUEST_URI'];
+
+if(isset($request)){
+    if($request=="/Projet1"){
+        $location = "./../Projet1.php";
+        require $location;
+    }
+    elseif($request=="/ListeFilm"){
+        $location = "./../ListeFilm.php";
+        require $location;
+    }
+    else{
+        require "temp.php";
+    }
+
+}
+
+?>
