1f408de60d525e8f728a295db756f44bc1b1a827
Steven200405 <huangsteven2004@gmail.com>
2024-11-13 11:30:39 +0000

    Update switch case

--- public/index.php
+++ public/index.php
@@ -2,18 +2,16 @@
 $request = $_SERVER['REQUEST_URI'];
 
 if(isset($request)){
-    if($request=="/Projet1"){
-        $location = "./../Projet1.php";
-        require $location;
-    }
-    elseif($request=="/ListeFilm"){
-        $location = "./../ListeFilm.php";
-        require $location;
-    }
-    else{
-        require "temp.php";
-    }
-
+    switch($request){
+        case "/Projet1":
+            $location = "./../Projet1.php";
+            require $location;
+            break;
+        case"/ListeFilm":
+            $location = "./../ListeFilm.php";
+            require $location;
+            break;
+        }
 }
 
 ?>
